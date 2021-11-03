1fe73c2d6e66f8fc103a07725e048a7013a7e2ed
Ken Cruz <ken@kencruz.ca>
2021-11-03 17:05:33 -0400

    implemented working wordSearch function with tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) return false;
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  for (let col = 0; col < letters[0].length; col++) {
+    const colLine = [];
+    for (const row of letters) {
+      colLine.push(row[col]);
+    }
+    if (colLine.join("").includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
